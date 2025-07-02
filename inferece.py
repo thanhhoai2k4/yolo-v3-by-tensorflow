3a31fa90ab6bad59c867b031e1748750fb3d5d01
Nguyen_Thanh_Hoai-DH52200700 <disbray100@gmail.com>
2025-07-02 21:17:21 +0700

    update

--- inferece.py
+++ inferece.py
@@ -1,7 +1,7 @@
 from yolov3.yolo_v3_model import *
 import os
 from yolov3.config import num_class, checkpoint_path
-xxx = "test/images"
+xxx = "val/images"
 xml_list = os.listdir(xxx) # lay danh sach cac file xml
 xml_list = [os.path.join(os.getcwd(),xxx,xml) for xml in xml_list]
 model = tf.keras.models.load_model(checkpoint_path)
